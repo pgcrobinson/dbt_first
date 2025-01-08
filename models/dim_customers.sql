6c006e45ebb930d5d3dce9e5948a218d0c4c8242
Peter Robinson <peter@pgcdatadesigns.com>
2025-01-08 15:56:20 +0000

    another commit

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -2,23 +2,15 @@
 
 with customers as (
 
-select
-id as customer_id,
-first_name,
-last_name 
-from
-jaffle_shop_customers
+select*
+from {{ ref('stg_jaffle_shop__customers')}}
 ),
 
 orders as (
 
-    select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
-    from
-    jaffle_shop_orders
+ select*
+ from {{ ref('stg_jaffle_shop__orders')}} 
+
 ),
 
 customer_orders as (
